dd963cac1fb06dddd3952bb4af34fa38c7c26abd
Arvind S <arvindsunil7@gmail.com>
2025-12-02 09:35:40 +0530

    Update git ignore

--- api_checker.py
+++
@@ -1,8 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyD9vBmPV4itxoCLxgPfFCJ2oHMYswrffWc")
-
-# List all available models
-for model in genai.list_models():
-    if 'generateContent' in model.supported_generation_methods:
-        print(model.name)
